9d1a7a86089d8015c31a4be622eb37095d58e4b0
safe2008 <boriphuth.sae@truedigital.com>
2021-04-09 00:10:45 +0700

    Update

--- src/main/scala/org/safe2008/app/Main.scala
+++ src/main/scala/org/safe2008/app/Main.scala
@@ -5,7 +5,7 @@ import org.safe2008.lib.Calculator
 object Main{
   def main(args: Array[String]): Unit = {
     var cal = new Calculator()
-    Console.println(cal.add(10, 100))
+    Console.println("This is result from lib :" , cal.add(10, 100))
     Console.println("RUN RUN RUN RUN")
     // Console.println(sys.env.get("GITHUB_TOKEN"))
   }
